a171c9cbd458ee1517865a110ac0aaca37fa9d16
subratrout <subratnrout@gmail.com>
2022-01-24 19:25:02 -0600

    Added code to remove the least size preference catgory and move or eliminate according to next position

--- src/VoteCounting.java
+++ src/VoteCounting.java
@@ -18,7 +18,7 @@ public class VoteCounting {
 
     private static void calculateVoteCount(List<Map<String, Integer>> allBallots){
         Map<String, List<Map<String, Integer>>> preferenceVsBallots = new LinkedHashMap<>();
-        System.out.println(allBallots);
+        System.out.println("All Ballots: " + allBallots);
         allBallots.stream().forEach(ballot -> {
             Integer minValAmongPreferences = ballot.values().stream().min(Comparator.comparingInt(Integer::intValue)).get();
             String optionText = null;
@@ -34,38 +34,62 @@ public class VoteCounting {
             preferenceVsBallots.get(optionText).add(ballot);
         });
 
-
+        evaluateResult(preferenceVsBallots, 1);
     }
 
-    private static boolean isResultAnnounced(Map<String, List<Map<String, Integer>>> preferenceVsBallots){
+    private static void evaluateResult(Map<String, List<Map<String, Integer>>> preferenceVsBallots, int round){
+        System.out.println("****************  Round" + round + "  **************");
         votingCountRoundEvaluationBasedOnPref(preferenceVsBallots);
 
         Integer totalActiveBallots = preferenceVsBallots.values().stream().map(v-> v.size()).collect(Collectors.summingInt(Integer::intValue));
-        System.out.println(totalActiveBallots);
+        System.out.println("Total Active Ballots: " + totalActiveBallots);
         int quota = (totalActiveBallots /2) + 1;
+        System.out.println("Quota = " + quota);
+
         boolean isResultAnnounced = false;
 
-        while(!isResultAnnounced){
-            for(Map.Entry<String, List<Map<String, Integer>>> entry: preferenceVsBallots.entrySet()){
-                String preferenceCategory = entry.getKey();
-                int size = entry.getValue().size();
-                if(size >= quota){
-                    System.out.println("The winner is: "+ preferenceCategory);
-                    isResultAnnounced = true;
-                    break;
-                }
+        for(Map.Entry<String, List<Map<String, Integer>>> entry: preferenceVsBallots.entrySet()){
+            String preferenceCategory = entry.getKey();
+            int size = entry.getValue().size();
+            if(size >= quota){
+                System.out.println("The winner is: "+ preferenceCategory);
+                isResultAnnounced = true;
+                break;
             }
         }
+
         if(!isResultAnnounced){
-            isResultAnnounced(preferenceVsBallots);
+            round++;
+            int minSize = Integer.MAX_VALUE;
+            String minSizePreferenceCategory = null;
+            // getting minimum size for each category
+            for(Map.Entry<String,List<Map<String, Integer>>> entry: preferenceVsBallots.entrySet()){
+                String preferenceCategory = entry.getKey();
+                int size = entry.getValue().size();
+                if(size < minSize){
+                    minSize = size;
+                    minSizePreferenceCategory = preferenceCategory;
+                }
+            }
+            List<Map<String,Integer>> minSizePreferenceCategoryBallots = preferenceVsBallots.get(minSizePreferenceCategory);
+            preferenceVsBallots.remove(minSizePreferenceCategory);
+            System.out.println("The candidate " + minSizePreferenceCategory + " is eliminated");
+
+            for(Map<String, Integer> ballot: minSizePreferenceCategoryBallots){
+                String optionText = ballot.entrySet().stream().filter(e-> e.getValue() >1).map(e-> e.getKey()).findFirst().get();
+                if(preferenceVsBallots.containsKey(optionText)){
+                    preferenceVsBallots.get(optionText).add(ballot);
+                }
+            }
+            evaluateResult(preferenceVsBallots, round);
         }
-        return isResultAnnounced;
+
     }
 
     private static void votingCountRoundEvaluationBasedOnPref(Map<String, List<Map<String, Integer>>> preferenceVsBallots){
         System.out.println("=========Printing our preferenceVsBallots =======");
         preferenceVsBallots.entrySet().stream().forEach(e->{
-            System.out.println("Preference category: " + e.getKey());
+            System.out.println("Preference category: " + e.getKey() + " [current vote= "+ e.getValue().size() + "]");
             e.getValue().stream().forEach(System.out::println);
         });
     }
